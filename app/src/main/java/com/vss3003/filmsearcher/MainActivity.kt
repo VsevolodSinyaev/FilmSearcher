9d2f5f1a8d050f3fbf93b6f83374cddaf40aa474
Vsevolod Sinyaev <vsevolods3003@gmail.com>
2021-10-09 23:39:29 +0300

    Added TextView, CardView with images, LinealLayout with buttons

--- app/src/main/java/com/vss3003/filmsearcher/MainActivity.kt
+++ app/src/main/java/com/vss3003/filmsearcher/MainActivity.kt
@@ -2,10 +2,32 @@ package com.vss3003.filmsearcher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_menu.setOnClickListener {
+            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+        }
+
+        button_watch_later.setOnClickListener {
+            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+        }
+
+        button_favorite.setOnClickListener {
+            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+        }
+
+        button_options.setOnClickListener {
+            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+        }
+
+        button_selected.setOnClickListener {
+            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+        }
     }
 }
